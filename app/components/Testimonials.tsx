e5ac6211af7d71b09b60460aaa3d639fdd1bc7bc
mikohan <angara99@gmail.com>
2025-12-01 20:50:28 -0800

    Starting testimonial job

---
+++ app/components/Testimonials.tsx
@@ -0,0 +1,21 @@
+function Testimonials() {
+  return (
+    <section className="bg-white">
+      <div className="container">
+        {/* Header and subheader */}
+        <div>
+          <h2 className="">What our customers say</h2>
+          <p>Our customer love us so much. We make them happy all the time.</p>
+        </div>
+        {/* Testimonials mapping */}
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </div>
+    </section>
+  );
+}
+export { Testimonials };
