228646ddc267a1fa7e03fc45a4b6c7dd845e8b99
mikohan <angara99@gmail.com>
2025-12-12 11:52:14 -0800

    Avatars comonent

---
+++ app/components/AvatarGroup.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+function AvatarGroup() {
+  const avatarArr = [
+    "https://img.daisyui.com/images/profile/demo/batperson@192.webp",
+    "https://img.daisyui.com/images/profile/demo/batperson@192.webp",
+    "https://img.daisyui.com/images/profile/demo/spiderperson@192.webp",
+  ];
+
+  return (
+    <div className="avatar-group -space-x-4 -md:space-x-6 h-8 md:h-auto">
+      {avatarArr.map((src, i) => (
+        <div key={i} className="avatar">
+          <div className="w-auto md:w-12">
+            <Image alt="Avatar" width={40} height={40} src={src} />
+          </div>
+        </div>
+      ))}
+      <div className="avatar avatar-placeholder">
+        <div className="bg-neutral text-neutral-content w-6 md:w-12 text-xs md:text-base">
+          <span>+99</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+export { AvatarGroup };
--- app/components/couchComponents/Hero.tsx
+++ app/components/couchComponents/Hero.tsx
@@ -1,20 +1,29 @@
 import { GoogleStars } from "../GoogleStars";
+import { AvatarGroup } from "../AvatarGroup";
+import VideoImage from "@/public/images/couch/hero.jpg";
+import Image from "next/image";
 
 function Hero() {
   return (
-    <section>
+    <section className="pt-8 pb-16">
       <div className="container px-6 mx-auto">
         <div>
-          <GoogleStars starsCount={5} rating="" />
+          <div>
+            <GoogleStars starsCount={5} rating="" />
+            <AvatarGroup />
+          </div>
+          <h1 className="text-3xl font-bold tracking-tight bg-linear-to-b from-couchDarkBlue to-couchGradientBlue text-transparent bg-clip-text">
+            Couch and Upholstery Steam Cleaning in LA
+          </h1>
+          <h3 className="text-lg font-blauerRegular text-couchDarkBlue/70 tracking-tight leading-4.5 mt-4">
+            We restore freshness, remove odors, and save you furniture time &
+            money.
+          </h3>
+          <button className="btn">Secure your appointment</button>
         </div>
-        <h1 className="text-3xl font-bold tracking-tight bg-linear-to-b from-couchDarkBlue to-couchGradientBlue text-transparent bg-clip-text">
-          Couch and Upholstery Steam Cleaning in LA
-        </h1>
-        <h3 className="text-lg font-blauerRegular text-couchDarkBlue/70 tracking-tight leading-4.5 mt-4">
-          We restore freshness, remove odors, and save you furniture time &
-          money.
-        </h3>
-        <button className="btn">Secure your appointment</button>
+        <div>
+          <Image src={VideoImage} alt="Hero image" className="mt-16 md:mt-0" />
+        </div>
       </div>
     </section>
   );
