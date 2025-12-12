7f3eabae2d8af70210a00cb22816afad052da3cb
mikohan <angara99@gmail.com>
2025-12-11 20:14:50 -0800

    Font set up

--- app/components/couchComponents/Header.tsx
+++ app/components/couchComponents/Header.tsx
@@ -1,4 +1,30 @@
-function Hero() {
-  return <div></div>;
+import Logo from "@/public/images/couch/logo.png";
+import Image from "next/image";
+import MenuIcon from "@/public/images/couch/hamburger.svg";
+
+function Header() {
+  return (
+    <header className="sticky top-0">
+      <div className="py-4">
+        <div className="container">
+          <div className="flex items-center justify-between px-6 border border-red-400">
+            <Image alt="Logo" src={Logo} width={40} height={40} />
+            <Image
+              className="md:hidden"
+              alt="Icon menu"
+              src={MenuIcon}
+              width={24}
+              height={24}
+            />
+            <nav className="hidden md:flex gap-6 items-center font-blauerSemibold text-couchDarkBlue/60">
+              <a href="#">Home</a>
+              <a href="#">Prices</a>
+              <a href="#">Contacts</a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
-export { Hero };
+export { Header };
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,5 +5,40 @@ import { GoogleStars } from "../components/GoogleStars";
 import { VideoComponent } from "../components/VideoComponent";
 
 export default function AboutPage() {
-  return <div className="container max-w-4xl mx-auto"></div>;
+  return (
+    <div className="container max-w-4xl mx-auto">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-10">
+        <div className="w-full md:w-1/2">
+          <div>
+            <GoogleStars
+              starsCount={5}
+              rating="4.99"
+              text="AVERAGE RATING"
+              inline={false}
+            />
+          </div>
+          <div>
+            <AvatarGroup />
+          </div>
+          <h1>About Us</h1>
+          <p>This is the about page content.</p>
+        </div>
+        <div className="relative w-full md:w-1/2 h-[350px] md:h-[600px]">
+          <Image
+            fetchPriority="high"
+            src={HeroImageDesktop}
+            priority
+            alt="some"
+            sizes="(max-width: 250px) 140px, (max-width: 768px) 100vw, (max-width: 1440px) 50vw, 33vw"
+            // sizes="(min-width: 780px) 479px, 100vw"
+            className="object-cover"
+            fill
+          />
+        </div>
+        <div>
+          <VideoComponent source="videos/test.mp4" autoPlay />
+        </div>
+      </div>
+    </div>
+  );
 }
