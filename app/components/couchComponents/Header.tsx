72768cac640e057ed90443aca449720e5d8aa845
mikohan <angara99@gmail.com>
2025-12-18 20:48:40 -0800

    added phone number

--- app/components/couchComponents/Header.tsx
+++ app/components/couchComponents/Header.tsx
@@ -1,6 +1,5 @@
 import Logo from "@/public/images/couch/logo.png";
 import Image from "next/image";
-import MenuIcon from "@/public/images/couch/hamburger.svg";
 
 function Header() {
   return (
@@ -9,25 +8,28 @@ function Header() {
         <div className="flex items-center justify-between px-6">
           <div className="inline-flex gap-0 items-center">
             <Image alt="Logo" src={Logo} width={64} height={64} />
-            <p className="text-2xl font-blauerRegular">
+
+            <p className="hidden md:block text-xl font-blauerRegular">
               Angara
               <span className="font-blauerSemibold text-couchBlue/80">
                 Steamers
               </span>
             </p>
+            <p className="text-xs md:hidden">
+              <span className="font-blauerRegular text-couchBlue">Angara</span>{" "}
+              Steamers
+            </p>
           </div>
-          <Image
-            className="md:hidden"
-            alt="Icon menu"
-            src={MenuIcon}
-            width={24}
-            height={24}
-          />
           <nav className="hidden md:flex gap-6 items-center font-blauerSemibold text-couchDarkBlue/60">
             <a href="#">Home</a>
             <a href="#">Prices</a>
             <a href="#">Contacts</a>
           </nav>
+          <div>
+            <p className="font-blauerMedium text-couchShadow">
+              <a href="tel:+12135987763">(213) 598 7763</a>
+            </p>
+          </div>
         </div>
       </div>
     </header>
