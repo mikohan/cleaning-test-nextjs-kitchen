23738cf56dabd0fb85386d47a0929ddac05661c5
mikohan <angara99@gmail.com>
2026-01-09 12:21:22 -0800

    Added new page with image

---
+++ app/components/couchComponents/HeroImage.tsx
@@ -0,0 +1,81 @@
+import dynamic from "next/dynamic";
+import { GoogleStars } from "../GoogleStars";
+import { AvatarGroup } from "../AvatarGroup";
+import { ModalDaisy } from "../ModalDaisy";
+import { ModalVideo } from "@/app/components/ModalVideo";
+import Image from "next/image";
+import MyImage from "@/public/images/couch/hero-me-color.png";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+// import { VideoComponent } from "../VideoComponent";
+// Load the video component without SSR to save main thread time
+const VideoComponent = dynamic(
+  () =>
+    import("@/app/components/VideoComponent").then((mod) => mod.VideoComponent),
+  {
+    ssr: false,
+  }
+);
+
+function HeroImage() {
+  return (
+    <section className="pt-8 md:pt-16 pb-16 bg-gradient">
+      <div className="container max-w-7xl mx-auto px-6">
+        {/* Wrapper for text and image */}
+        <div className="md:flex justify-center">
+          {/* Text and buttons section */}
+          <div className="text-center md:flex-1">
+            <div className="inline-flex md:flex-col items-center md:gap-2 ">
+              <GoogleStars starsCount={5} rating="" />
+              <AvatarGroup />
+            </div>
+            {/* Wrapper for text and button without rating */}
+            <div className="flex flex-col md:items-center md:w-full">
+              <RoughNotationGroup show={true}>
+                <h1 className="md:leading-14 mt-4 md:mt-32 text-3xl md:text-5xl font-bold font-gradient">
+                  Couch and Upholstery{" "}
+                  <RoughNotation
+                    type="circle"
+                    color="#51a2ff"
+                    padding={1}
+                    strokeWidth={2}
+                  >
+                    Professional{" "}
+                  </RoughNotation>
+                  Steam Cleaning in LA
+                </h1>
+              </RoughNotationGroup>
+              <h2 className="mx-8 text-lg md:text-2xl font-blauerRegular text-couchDarkBlue/70 tracking-tight leading-4.5 mt-4 md:mt-8">
+                We restore freshness, remove odors, and save you furniture time
+                & money. Professional, safe, fast.
+              </h2>
+              <div className="w-full md:flex md:gap-4 mt-16 md:mt-40 md:items-center">
+                <ModalDaisy />
+                <ModalVideo />
+              </div>
+            </div>
+          </div>
+          {/* Image container wrapper */}
+          <div className="mt-16 md:mt-0 md:flex-1 flex justify-center items-center">
+            <div className="relative aspect-1080/1350 w-full md:w-[80%]">
+              {/* <VideoComponent
+                source="/videos/LandPage1.mp4"
+                width="1080"
+                heigh="1350"
+              /> */}
+
+              <Image
+                fetchPriority="high"
+                src={MyImage}
+                priority
+                alt="some"
+                className="rounded-4xl object-cover"
+                fill
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export { HeroImage };
---
+++ app/couch/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { CallToAction } from "@/app/components/couchComponents/CallToAction";
+import { Header } from "@/app/components/couchComponents/Header";
+import { HeroImage } from "@/app/components/couchComponents/HeroImage";
+import { Prices } from "@/app/components/couchComponents/Prices";
+import { Problem } from "@/app/components/couchComponents/Problem";
+import { Team } from "@/app/components/couchComponents/Team";
+import { WhyUs } from "@/app/components/couchComponents/WhyUs";
+import { Footer } from "@/app/components/Footer";
+import { LogoTicker } from "@/app/components/LogoTicker";
+import { Testimonials } from "@/app/components/Testimonials";
+import { useLenis } from "lenis/react";
+
+export default function AboutPage() {
+  const lenis = useLenis(); //new Lenis();
+  const handleClick = () => {
+    lenis?.scrollTo(0, {
+      duration: 2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+  };
+  return (
+    <main className="relative font-blauerRegular text-couchDarkBlue">
+      <Header />
+      <HeroImage />
+      <LogoTicker />
+      <Problem />
+      <WhyUs />
+      <section id="prices">
+        <Prices />
+      </section>
+      <CallToAction className="py-16" />
+      <section id="reviews">
+        <Testimonials />
+      </section>
+      <Team />
+      <CallToAction className="bg-linear-to-t from-white to-couchLightBlue mb-16" />
+      <Footer handleClick={handleClick} />
+    </main>
+  );
+}
